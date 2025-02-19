63ffe0eaffaf621c8c66a870f15531a4477f479e
locoioioi <118866346+locoioioi@users.noreply.github.com>
2025-02-19 14:07:41 +0700

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="text-2xl">
+			hello World
+			<Button>Submit</Button>
+		</div>
+	);
+}
